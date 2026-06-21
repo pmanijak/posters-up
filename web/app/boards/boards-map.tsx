32706055d9bceddb9171b9026ba0ffeabc061213
Philip Manijak <130693+pmanijak@users.noreply.github.com>
2026-06-20 19:53:35 -0700

    map: use voyager tiles

--- web/app/boards/boards-map.tsx
+++ web/app/boards/boards-map.tsx
@@ -144,7 +144,7 @@ export default function BoardsMap({
       */}
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
-        url="https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png"
+        url="https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png"
         subdomains="abcd"
         maxZoom={20}
       />
