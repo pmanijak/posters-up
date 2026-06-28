b93d63b19e403bf89db2272408ac46393cd9c247
Philip Manijak <130693+pmanijak@users.noreply.github.com>
2026-06-28 14:25:23 -0700

    about: use apos char

--- web/app/components/about-card.tsx
+++ web/app/components/about-card.tsx
@@ -14,11 +14,11 @@ export function AboutCard() {
         </p>
         <p className="text-sm leading-relaxed text-content-secondary">
           This project was started in Olympia, Washington, to see if we can find
-          out what's happening by taking photos of the bulletin boards downtown.
+          out what&apos;s happening by taking photos of the bulletin boards downtown.
           It turns out, we can.
         </p>
         <p className="text-sm leading-relaxed text-content-secondary">
-          Anybody can add photos, so even if you're not in Olympia,
+          Anybody can add photos, so even if you&apos;re not in Olympia,
           it can work for you if you have lots of posters in your neighborhood.
         </p>
 
